978e634e594bff704478ba2a032e5ee560e33681
shok1122 <shok1122@zohomail.com>
2020-12-09 09:56:09 +0000

    Just copy the bin directory

--- script/install-fabric.sh
+++ script/install-fabric.sh
@@ -1,7 +1,10 @@
 #/bin/sh
 
 if [ "binary" = "$1" ]; then
+    pushd /tmp
     curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.1.1 1.4.7 0.4.20 -ds
+    popd
+    cp -r /tmp/bin .
 fi
 
 if [ "docker" = "$1" ]; then

8bf3a187c18dcdca624511c8c58bbfd534aeebe4
shok1122 <shok1122@zohomail.com>
2020-12-09 13:22:40 +0000

    Added the process of creating a channel

--- network.py
+++ network.py
@@ -26,11 +26,21 @@ with open('./secret/connection_list.yaml') as f:
 mode = sys.argv[1]
 
 all_conf = None
-
 with open('env.yaml') as f:
     all_conf = yaml.safe_load(f)
 
+g_pwd = os.getcwd()
+g_domain  = all_conf['domain']
+g_channel = all_conf['channel']
+g_path_orderer_ca = f'{g_pwd}/organizations/ordererOrganizations/{g_domain}/orderers/orderer.{g_domain}/msp/tlscacerts/tlsca.{g_domain}-cert.pem'
+
 env = Environment(loader=FileSystemLoader('template'))
+
+def set_org_env(org):
+    os.environ['CORE_PEER_LOCALMSPID'] = org
+    os.environ['CORE_PEER_TLS_ROOTCERT_FILE'] = f'{g_pwd}/organizations/peerOrganizations/{org}.{g_domain}/peers/peer0.{org}.{g_domain}/tls/ca.crt'
+    os.environ['CORE_PEER_MSPCONFIGPATH'] = f'{g_pwd}/organizations/peerOrganizations/{org}.{g_domain}/users/Admin@{org}.{g_domain}/msp'
+    os.environ['CORE_PEER_ADDRESS'] = f'peer0-{org}.{g_domain}:7051'
 
 def render(file_name, conf):
     template = env.get_template(file_name)
@@ -131,6 +141,42 @@ def distribution():
                     scpc.put(
                         files=f"cache/docker-compose-{peer}-{org}.{domain}.yaml",
                         remote_path='/tmp')
+                    scpc.put(
+                        files=f"conf/configtx.yaml",
+                        remote_path='/tmp')
+
+def create_channel_tx():
+    command = f' \
+        bin/configtxgen \
+            -profile SampleSingleMSPChannel \
+            -outputCreateChannelTx ./channel-artifacts/{g_channel}.tx \
+            -channelID {g_channel}'
+    print(command)
+    subprocess.call(command, shell=True)
+
+def create_anchor_peer_tx():
+    for org_conf in all_conf['orgs']:
+        org = org_conf['name']
+        command = f' \
+            bin/configtxgen \
+                -profile SampleSingleMSPChannel \
+                -outputAnchorPeersUpdate ./channel-artifacts/{org}-anchors.tx \
+                -channelID {g_channel} \
+                -asOrg {org}'
+        subprocess.call(command, shell=True)
+
+def create_channel():
+    org = all_conf['orgs'][0]['name']
+    set_org_env(org)
+    command = f" \
+        bin/peer channel create \
+            -o orderer.{g_domain}:7050 \
+            -c {g_channel} \
+            -f ./channel-artifacts/{g_channel}.tx \
+            --outputBlock ./channel-artifacts/{g_channel}.block \
+            --tls \
+            --cafile {g_path_orderer_ca}"
+    subprocess.call(command, shell=True)
 
 def network_up():
     subprocess.call('docker-compose -f docker/docker-compose.yaml up -d', shell=True)
@@ -145,6 +191,10 @@ elif mode == "packaging":
     packing_conf_r()
 elif mode == "distribution":
     distribution()
+elif mode == "channel":
+    create_channel_tx()
+    create_anchor_peer_tx()
+    create_channel()
 elif mode == "up":
     network_up()
 elif mode == "all":

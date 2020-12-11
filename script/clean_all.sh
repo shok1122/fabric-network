138dd6f9745d670dab17533242a1b7e67898ab33
shok1122 <shok1122@zohomail.com>
2020-12-11 07:43:47 +0000

    Added the comments

--- script/clean_all.sh
+++ script/clean_all.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+echo "remove cache/*..."
 rm -rf cache/*
+echo "remove channel-artifacts/*..."
 rm -rf channel-artifacts/*
+echo "remove conf/*..."
 rm -rf conf/*
+echo "remove docker/*..."
 rm -rf docker/*
+echo "remove organizations/*..."
 rm -rf organizations/*
+echo "remove system-genesis-block/*..."
 rm -rf system-genesis-block/*

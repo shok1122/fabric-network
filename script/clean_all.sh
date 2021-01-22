e06359446d4db4756763f55679e75e741175f385
shok1122 <shok1122@zohomail.com>
2021-01-22 10:54:24 +0000

    Moved the docker-composed down command to clean_all.sh

--- script/clean_all.sh
+++ script/clean_all.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
-echo "remove cache/*..."
+echo 'stop docker images...'
+docker-compose -f docker/docker-compose.yaml down --volumes
+
+echo 'remove cache/*...'
 rm -rf cache/*
-echo "remove channel-artifacts/*..."
+echo 'remove channel-artifacts/*...'
 rm -rf channel-artifacts/*
-echo "remove conf/*..."
+echo 'remove conf/*...'
 rm -rf conf/*
-echo "remove docker/*..."
+echo 'remove docker/*...'
 rm -rf docker/*
-echo "remove organizations/*..."
+echo 'remove organizations/*...'
 rm -rf organizations/*
-echo "remove system-genesis-block/*..."
+echo 'remove system-genesis-block/*...'
 rm -rf system-genesis-block/*

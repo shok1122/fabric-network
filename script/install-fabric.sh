6a5960a546fceeb63a809b8cddd4344871708f0f
shok1122 <shok1122@zohomail.com>
2021-02-15 11:00:46 +0000

    Upgrade Fabric

--- script/install-fabric.sh
+++ script/install-fabric.sh
@@ -2,11 +2,11 @@
 
 if [ "binary" = "$1" ]; then
     pushd /tmp
-    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.1.1 1.4.7 0.4.20 -ds
+    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.3.0 1.4.9 -ds
     popd
     cp -r /tmp/bin .
 fi
 
 if [ "docker" = "$1" ]; then
-    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.1.1 1.4.7 0.4.20 -bs
+    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.3.0 1.4.9 -bs
 fi

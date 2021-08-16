906c86f596d235485978ced0b2b4d55fc44c5a52
shok1122 <shok1122@zohomail.com>
2021-08-16 22:52:54 +0900

    Added project management

--- ctrl.py
+++ ctrl.py
@@ -10,36 +10,38 @@ import yaml
 from jinja2 import Template, Environment, FileSystemLoader
 from pprint import pprint
 
-ENV_PATH = os.getenv('PATH')
-os.environ['PATH'] = os.getcwd() + '/bin' + ':' + ENV_PATH
-os.environ['FABRIC_CFG_PATH'] = './conf'
-os.environ['CORE_PEER_TLS_ENABLED'] = 'true'
-print(os.getenv('PATH'))
-
-connection_list = None
-
-with open('./secret/connection_list.yaml') as f:
-    connection_list = yaml.safe_load(f)
-
 mode = sys.argv[1]
+proj = sys.argv[2]
+
+g_pwd = os.getcwd() + '/projects/' + proj
+print(g_pwd)
+
+ENV_PATH = os.getenv('PATH')
+os.environ['PATH'] = f"{g_pwd}/bin:{ENV_PATH}"
+os.environ['FABRIC_CFG_PATH'] = f'{g_pwd}/conf'
+os.environ['CORE_PEER_TLS_ENABLED'] = 'true'
 
 gconf = None
-with open('config-network.yaml') as f:
-    gconf = yaml.safe_load(f)
-
-g_pwd = os.getcwd()
-g_orderer_domain = gconf['orderer']['domain']
-g_channel = gconf['channel']
-g_path_orderer_ca = f'{g_pwd}/conf/organizations/ordererOrganizations/{g_orderer_domain}/orderers/orderer.{g_orderer_domain}/msp/tlscacerts/tlsca.{g_orderer_domain}-cert.pem'
+g_orderer_domain = None
+g_path_orderer_ca = None
 
 env = Environment(loader=FileSystemLoader('template'))
 
 def print_bannar(text):
     print(f"> {text}")
 
+def load_network_config():
+    global gconf
+    global g_orderer_domain
+    global g_path_orderer_ca
+    with open(f'{g_pwd}/config-network.yaml') as f:
+        gconf = yaml.safe_load(f)
+    g_orderer_domain = gconf['orderer']['domain']
+    g_path_orderer_ca = f'{g_pwd}/conf/organizations/ordererOrganizations/{g_orderer_domain}/orderers/orderer.{g_orderer_domain}/msp/tlscacerts/tlsca.{g_orderer_domain}-cert.pem'
+
 def load_crypto_config_org():
-    if os.path.exists('./conf/crypto-config-org.yaml'):
-        with open('./conf/crypto-config-org.yaml') as f:
+    if os.path.exists(f'{g_pwd}/conf/crypto-config-org.yaml'):
+        with open(f'{g_pwd}/conf/crypto-config-org.yaml') as f:
             return yaml.safe_load(f)
     return None
 
@@ -68,32 +70,50 @@ def save_file(path, data):
 
 def install():
     # install binaries
-    files = os.listdir('bin')
+    files = os.listdir(f'{g_pwd}/bin')
     files = [ f for f in files if not f.startswith('.') ]
     if not files:
-        subprocess.call('script/install-fabric.sh binary', shell=True)
+        subprocess.call(f'script/install-fabric.sh binary {g_pwd}', shell=True)
 
     # install docker images
     subprocess.call('script/install-fabric.sh docker', shell=True)
+
+def create_project(_org_num):
+    os.mkdir(g_pwd)
+    os.mkdir(g_pwd + '/bin')
+    os.mkdir(g_pwd + '/cache')
+    os.mkdir(g_pwd + '/channel-artifacts')
+    os.mkdir(g_pwd + '/conf')
+    os.mkdir(g_pwd + '/conf/organizations')
+    os.mkdir(g_pwd + '/system-genesis-block')
+
+    data = {
+        'org_num': int(_org_num)
+    }
+
+    ret_text = render('config-network.yaml.tmpl', data)
+    save_file(f'{g_pwd}/config-network.yaml', ret_text)
+
+    install()
 
 def create_settings():
 
     print_bannar('create settings')
 
     ret_text = render('configtx.yaml.tmpl', gconf)
-    save_file('conf/configtx.yaml', ret_text)
+    save_file(f'{g_pwd}/conf/configtx.yaml', ret_text)
 
     ret_text = render('core.yaml.tmpl', gconf)
-    save_file('conf/core.yaml', ret_text)
+    save_file(f'{g_pwd}/conf/core.yaml', ret_text)
 
     ret_text = render('crypto-config-orderer.yaml.tmpl', gconf)
-    save_file('conf/crypto-config-orderer.yaml', ret_text)
+    save_file(f'{g_pwd}/conf/crypto-config-orderer.yaml', ret_text)
 
     ret_text = render('crypto-config-org.yaml.tmpl', gconf)
-    save_file('conf/crypto-config-org.yaml', ret_text)
+    save_file(f'{g_pwd}/conf/crypto-config-org.yaml', ret_text)
 
     ret_text = render('docker-compose.yaml.tmpl', gconf)
-    save_file('docker/docker-compose.yaml', ret_text)
+    save_file(f'{g_pwd}/docker-compose.yaml', ret_text)
 
     data = {}
     for o in gconf['orgs']:
@@ -102,28 +122,28 @@ def create_settings():
         for p in o['peers']:
             data['peer'] = p['name']
             ret_text = render('config-peer.yaml.tmpl', data)
-            save_file(f"cache/config-peer-{p['name']}.{o['domain']}.yaml", ret_text)
+            save_file(f"{g_pwd}/cache/config-peer-{p['name']}.{o['domain']}.yaml", ret_text)
 
 def create_org():
 
     print_bannar('create org')
 
-    path = 'conf/organizations/ordererOrganizations'
+    path = f'{g_pwd}/conf/organizations/ordererOrganizations'
     if os.path.exists(path):
         shutil.rmtree(path)
 
-    path = 'conf/organizations/peerOrganizations'
+    path = f'{g_pwd}/conf/organizations/peerOrganizations'
     if os.path.exists(path):
         shutil.rmtree(path)
 
-    subprocess.call('cryptogen generate --config=./conf/crypto-config-orderer.yaml --output=conf/organizations', shell=True)
-    subprocess.call('cryptogen generate --config=./conf/crypto-config-org.yaml --output=conf/organizations', shell=True)
+    subprocess.call(f'cryptogen generate --config={g_pwd}/conf/crypto-config-orderer.yaml --output={g_pwd}/conf/organizations', shell=True)
+    subprocess.call(f'cryptogen generate --config={g_pwd}/conf/crypto-config-org.yaml --output={g_pwd}/conf/organizations', shell=True)
 
 def create_consortium():
 
     print_bannar('create consortium')
 
-    subprocess.call('configtxgen -profile DefaultProfile -channelID system-channel -outputBlock ./system-genesis-block/genesis.block', shell=True)
+    subprocess.call(f'configtxgen -profile DefaultProfile -channelID system-channel -outputBlock {g_pwd}/system-genesis-block/genesis.block', shell=True)
 
 def make_tarfile(output_filename, source_dir, peer, domain):
     with tarfile.open(output_filename, "w:gz") as tar:
@@ -131,8 +151,8 @@ def make_tarfile(output_filename, source_dir, peer, domain):
 
 def packing_conf(peer, domain):
     print(f"> packing conf ({peer} + '.' + {domain})")
-    path = "conf/organizations"
-    tar_file = f"cache/{peer}.{domain}.tar.gz"
+    path = f"{g_pwd}/conf/organizations"
+    tar_file = f"{g_pwd}/cache/{peer}.{domain}.tar.gz"
     make_tarfile(tar_file, path, peer, domain)
 
 def packing_conf_r(crypto_config_org):
@@ -144,6 +164,9 @@ def packing_conf_r(crypto_config_org):
             packing_conf(peer, org_conf['domain'])
 
 def distribution(crypto_config_org):
+    connection_list = None
+    with open(f'{g_pwd}/connection_list.yaml') as f:
+        connection_list = yaml.safe_load(f)
     print_bannar('distribution')
     for x in crypto_config_org['PeerOrgs']:
         org = x['Name']
@@ -162,111 +185,54 @@ def distribution(crypto_config_org):
                 with scp.SCPClient(sshc.get_transport()) as scpc:
                     print(hostname)
                     scpc.put(
-                        files=f"cache/{peer}.{domain}.tar.gz",
+                        files=f"{g_pwd}/cache/{peer}.{domain}.tar.gz",
                         remote_path='/tmp/organizations.tar.gz')
                     scpc.put(
-                        files=f"cache/config-peer-{peer}.{domain}.yaml",
+                        files=f"{g_pwd}/cache/config-peer-{peer}.{domain}.yaml",
                         remote_path='/tmp/config-peer.yaml')
                     scpc.put(
                         files=f"config-network.yaml",
                         remote_path='/tmp/config-network.yaml')
                     scpc.put(
-                        files=f"conf/configtx.yaml",
+                        files=f"{g_pwd}/conf/configtx.yaml",
                         remote_path='/tmp/configtx.yaml')
                     scpc.put(
-                        files=f"conf/core.yaml",
+                        files=f"{g_pwd}/conf/core.yaml",
                         remote_path='/tmp/core.yaml')
 
-def create_channel_tx():
-    command = f' \
-        bin/configtxgen \
-            -profile SampleSingleMSPChannel \
-            -outputCreateChannelTx ./channel-artifacts/{g_channel}.tx \
-            -channelID {g_channel}'
-    print(command)
-    subprocess.call(command, shell=True)
-
-def create_anchor_peer_tx():
-    for org_conf in gconf['orgs']:
-        org = org_conf['name']
-        command = f' \
-            bin/configtxgen \
-                -profile SampleSingleMSPChannel \
-                -outputAnchorPeersUpdate ./channel-artifacts/{org}-anchors.tx \
-                -channelID {g_channel} \
-                -asOrg {org}'
-        subprocess.call(command, shell=True)
-
-def create_channel():
-    print('------------------------------------')
-    print(' create channel')
-    print('------------------------------------')
-    org = gconf['orgs'][0]['name']
-    set_org_env(org)
-    command = f" \
-        bin/peer channel create \
-            -o orderer.{g_orderer_domain}:7050 \
-            -c {g_channel} \
-            -f ./channel-artifacts/{g_channel}.tx \
-            --outputBlock ./channel-artifacts/{g_channel}.block \
-            --tls \
-            --cafile {g_path_orderer_ca}"
-    subprocess.call(command, shell=True)
-
-def join_channel():
-    print('------------------------------------')
-    print(' join channel')
-    print('------------------------------------')
-    for org_conf in gconf['orgs']:
-        org = org_conf['name']
-        set_org_env(org)
-        command = f" \
-            bin/peer channel join \
-                -b ./channel-artifacts/{g_channel}.block"
-        subprocess.call(command, shell=True)
-
-def update_anchor_peers():
-    print('------------------------------------')
-    print(' update anchor peers')
-    print('------------------------------------')
-    for org_conf in gconf['orgs']:
-        org = org_conf['name']
-        set_org_env(org)
-        command = f" \
-            bin/peer channel update \
-                -o orderer.{g_orderer_domain}:7050 \
-                -c {g_channel} \
-                -f ./channel-artifacts/{org}-anchors.tx \
-                --tls \
-                --cafile {g_path_orderer_ca}"
-        subprocess.call(command, shell=True)
-
 def network_up():
-    subprocess.call('docker-compose -f docker/docker-compose.yaml up -d', shell=True)
+    subprocess.call('docker-compose -f {g_pwd}/docker-compose.yaml up -d', shell=True)
 
 def network_down():
-    subprocess.call('docker-compose -f docker/docker-compose.yaml down', shell=True)
+    subprocess.call('docker-compose -f {g_pwd}/docker-compose.yaml down', shell=True)
 
 def clean():
     subprocess.call('script/clean_all.sh', shell=True)
 
-if mode == "install":
-    install()
+if mode == "create-project":
+    org_num = sys.argv[3]
+    create_project(org_num)
 elif mode == "create-consortium":
+    load_network_config()
     create_settings()
     create_org()
     create_consortium()
 elif mode == "packaging":
+    load_network_config()
     crypto_config_org = load_crypto_config_org()
     packing_conf_r(crypto_config_org)
 elif mode == "distribution":
+    load_network_config()
     crypto_config_org = load_crypto_config_org()
     distribution(crypto_config_org)
 elif mode == "up":
+    load_network_config()
     network_up()
 elif mode == "down":
+    load_network_config()
     network_down()
 elif mode == "startup-network":
+    load_network_config()
     create_settings()
     create_org()
     create_consortium()
@@ -274,12 +240,8 @@ elif mode == "startup-network":
     packing_conf_r(crypto_config_org)
     distribution(crypto_config_org)
     network_up()
-elif mode == "startup-channel":
-    create_channel_tx()
-    create_anchor_peer_tx()
-    create_channel()
-    #join_channel()
-    #update_anchor_peers()
 elif mode == "clean":
     clean()
+else:
+    print_bannar(f"unknown mode: {mode}")
 
--- script/install-fabric.sh
+++ script/install-fabric.sh
@@ -1,10 +1,11 @@
 #/bin/sh
 
 if [ "binary" = "$1" ]; then
-    pushd /tmp
-    curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.3.0 1.4.9 -ds
-    popd
-    cp -r /tmp/bin .
+    (
+        cd /tmp
+        curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.3.0 1.4.9 -ds
+    )
+    cp -r /tmp/bin $2
 fi
 
 if [ "docker" = "$1" ]; then
